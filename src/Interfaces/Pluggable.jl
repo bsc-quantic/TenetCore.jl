4f29e8d36c44a919b1a87cac6fe841355f33ca09
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-05-06 12:31:18 +0200

    temporarily comment `plugs`, `plug`

--- src/Interfaces/Pluggable.jl
+++ src/Interfaces/Pluggable.jl
@@ -41,14 +41,14 @@ function adjoint_plugs! end
 
 # implementation
 ## `plugs`
-plugs(tn; kwargs...) = plugs(sort_nt(values(kwargs)), tn)
-plugs(::@NamedTuple{}, tn) = all_plugs(tn)
-plugs(kwargs::NamedTuple{(:set,)}, tn) = plugs_set(tn, kwargs.set)
+# plugs(tn; kwargs...) = plugs(sort_nt(values(kwargs)), tn)
+# plugs(::@NamedTuple{}, tn) = all_plugs(tn)
+# plugs(kwargs::NamedTuple{(:set,)}, tn) = plugs_set(tn, kwargs.set)
 
 ## `plug`
-plug(tn::AbstractTensorNetwork; kwargs...) = plug(sort_nt(values(kwargs)), tn)
-plug(::NamedTuple{(:at,)}, tn) = plug_at(tn, kwargs.at)
-plug(::NamedTuple{(:like,)}, tn) = plug_like(tn, kwargs.like)
+# plug(tn::AbstractTensorNetwork; kwargs...) = plug(sort_nt(values(kwargs)), tn)
+# plug(::NamedTuple{(:at,)}, tn) = plug_at(tn, kwargs.at)
+# plug(::NamedTuple{(:like,)}, tn) = plug_like(tn, kwargs.like)
 
 ## `all_plugs`
 all_plugs(tn) = all_plugs(tn, delegates(Pluggable(), tn))
@@ -67,7 +67,7 @@ end
 hasplug(tn, plug) = any(Base.Fix1(is_plug_equal, plug), links_iter(tn))
 
 ## `nplugs`
-nplugs(tn; kwargs...) = length(plugs(tn; kwargs...))
+nplugs(tn; kwargs...) = length(all_plugs(tn; kwargs...))
 
 ## `plugs_like`
 plugs_like(tn, plug) = links_like(is_plug_equal, tn, plug)
@@ -96,25 +96,25 @@ plugs_set_outputs(tn) = filter(t -> isplug(t) && !isdual(t), all_links_iter(tn))
 
 ## `inds_set` extensions
 inds_set(tn, ::Val{:physical}) = inds_set_physical(tn)
-inds_set_physical(tn) = Index[ind_at(tn, i) for i in plugs(tn)]
+inds_set_physical(tn) = Index[ind_at(tn, i) for i in all_plugs(tn)]
 
 inds_set(tn, ::Val{:virtual}) = inds_set_virtual(tn)
-inds_set_virtual(tn) = setdiff(all_inds(tn), Index[ind_at(tn, i) for i in plugs(tn; set=:physical)])
+inds_set_virtual(tn) = setdiff(all_inds(tn), Index[ind_at(tn, i) for i in plugs_set(tn, :physical)])
 
 inds_set(tn, ::Val{:inputs}) = inds_set_inputs(tn)
-inds_set_inputs(tn) = Index[ind_at(tn, i) for i in plugs(tn; set=:inputs)]
+inds_set_inputs(tn) = Index[ind_at(tn, i) for i in plugs_set(tn, :inputs)]
 
 inds_set(tn, ::Val{:outputs}) = inds_set_outputs(tn)
-inds_set_outputs(tn) = Index[ind_at(tn, i) for i in plugs(tn; set=:outputs)]
+inds_set_outputs(tn) = Index[ind_at(tn, i) for i in plugs_set(tn, :outputs)]
 
 ## `adjoint_plugs!`
 function adjoint_plugs!(tn)
     # update plug information and rename inner indices
     # generate mapping
-    mapping = Dict(plug => ind(tn; at=plug) for plug in plugs(tn))
+    mapping = Dict(plug => ind(tn; at=plug) for plug in all_plugs(tn))
 
     # remove sites preemptively to avoid issues on renaming
-    for plug_tag in plugs(tn)
+    for plug_tag in all_plugs(tn)
         untag!(tn, plug_tag)
     end
 
