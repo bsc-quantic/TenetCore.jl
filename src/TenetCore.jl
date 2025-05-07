062696ac48283f0a1d5d8a21aac77fc4f8a605f4
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-05-07 09:04:24 +0200

    bump version to 0.2.0

--- src/TenetCore.jl
+++ src/TenetCore.jl
@@ -31,7 +31,7 @@ export inds, ind, hasind, ninds, all_inds, all_inds_iter, replace_ind!
 export tensors_with_inds, tensors_contain_inds, tensors_intersect_inds
 export inds_set, inds_parallel_to
 export size_inds, size_ind
-export arrays
+export arrays, contract
 
 # from Taggable
 export sites, site, hassite, nsites, all_sites, sites_like, site_like
