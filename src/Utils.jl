e86c213331ac21caed57ef648eeb8d8be00bfd6b
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-05-16 11:43:19 +0200

    Fix `sort_nt` interference with Reactant piracy on `sort`

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,5 +1,6 @@
 # from https://discourse.julialang.org/t/sort-keys-of-namedtuple/94630/3
 @generated sort_nt(nt::NamedTuple{KS}) where {KS} = :(NamedTuple{$(Tuple(sort(collect(KS))))}(nt))
+sort_nt(nt::@NamedTuple{}) = nt
 
 mutable struct CachedField{T}
     isvalid::Bool
