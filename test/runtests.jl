c36c9a1d6d27ca950c5f8038759b7760cd0a82f7
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-05-16 17:25:45 +0200

    Isolate testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using Test
 using SafeTestsets
 
 @testset "Unit" verbose = true begin
-    @testset "TensorNetwork" verbose = true include("unit/tensor_network.jl")
-    @testset "Taggable" verbose = true include("unit/taggable.jl")
+    @safetestset "TensorNetwork" include("unit/tensor_network.jl")
+    @safetestset "Taggable" include("unit/taggable.jl")
     @safetestset "Pluggable" include("unit/pluggable.jl")
 end
 
