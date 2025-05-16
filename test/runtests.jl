9cda2ff4c966a9c46023c42f7e78bd76c293fd45
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-05-16 16:05:08 +0200

    Test Reactant integration

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,7 @@ using SafeTestsets
     @testset "Taggable" verbose = true include("unit/taggable.jl")
     @safetestset "Pluggable" include("unit/pluggable.jl")
 end
+
+@testset "Integration" verbose = true begin
+    @safetestset "Reactant" include("integration/reactant.jl")
+end
